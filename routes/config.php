b5611a488a375381d28e7ad962bd6d84f103c413
mess-dev <stephane.maissin@gmail.com>
2022-05-03 18:23:55 +0100

    Other

--- routes/config.php
+++ routes/config.php
@@ -1,9 +1,9 @@
 <?php
 
-Route::middleware(['web', 'auth:admin'])->get('/setup/init/config', function(){
-	return view('init::config');
+Route::middleware(['web', 'auth:admin'])->get('/setup/init/config', function () {
+    return view('init::config');
 })->name('setup.init.config');
 
-Route::get('install/warning', function(){
-	abort('403', 'Not installed');
+Route::get('install/warning', function () {
+    abort('403', 'Not installed');
 })->name('install.warning');
--- routes/setup.php
+++ routes/setup.php
@@ -5,10 +5,10 @@ use Eutranet\Init\Http\Controllers\InstallStatusController;
 use Eutranet\Init\Http\Controllers\MenuController;
 use Eutranet\Init\Http\Controllers\MediaController;
 
-Route::middleware(['web', 'auth:admin', 'role:super-admin'])->prefix('setup')->name('setup.')->group(function(){
-	Route::resource('install_statuses', InstallStatusController::class)->names('install-statuses');
-	Route::resource('language-lines', LanguageLineController::class)->names('language-lines');
-	Route::resource('media', MediaController::class)->names('media');
-	Route::resource('menus', MenuController::class)->names('menus');
-	Route::resource('users', UserController::class)->names('users');
+Route::middleware(['web', 'auth:admin', 'role:super-admin'])->prefix('setup')->name('setup.')->group(function () {
+    Route::resource('install_statuses', InstallStatusController::class)->names('install-statuses');
+    Route::resource('language-lines', LanguageLineController::class)->names('language-lines');
+    Route::resource('media', MediaController::class)->names('media');
+    Route::resource('menus', MenuController::class)->names('menus');
+    Route::resource('users', UserController::class)->names('users');
 });
