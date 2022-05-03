44cf06f5dcd7886440ea5cac9a8da64f7509d589
mess-dev <stephane.maissin@gmail.com>
2022-05-03 18:30:04 +0100

    Other

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,16 @@ use Illuminate\Foundation\Application;
 use Illuminate\View\Factory;
 
 Route::middleware(['guest', 'web'])->group(function () {
+    Route::redirect(/**
+     * @return Application|Factory|\Illuminate\Contracts\View\View
+     */ '/', 'welcome');
 
     Route::get(/**
      * @return Application|Factory|\Illuminate\Contracts\View\View
-     */ '/', function () {
+     */ 'welcome', function () {
         return view('welcome');
         //	return redirect('/welcome');
     })->name('welcome');
-
 
     Route::get('init', function () {
         return view('init::install', ['message' => 'Welcome to Eutranet\'s initalizer']);
